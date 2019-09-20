c431df707e317a84914a514a07c729ce17f6f216
Veselin Veselinov <43546748+ves00@users.noreply.github.com>
2019-09-20 11:32:29 +0200

    Update i18next.php

--- src/i18next.php
+++ src/i18next.php
@@ -1,13 +1,6 @@
 <?php
 
-/*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <github.com/Mika-> wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return
- * ----------------------------------------------------------------------------
- */
+
 
 namespace Aiken\i18next;
 
