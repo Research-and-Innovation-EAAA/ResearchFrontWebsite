7be0409ef50d260b79ec42f82e41070871b7b82f
Morten Mathiasen <morten@tranebaerkaeret.dk>
2019-11-01 07:03:07 +0100

    Remove and change database credentials

---
+++ src/credentials.template.php
@@ -0,0 +1,7 @@
+<?php
+
+$GLOBALS['database_host'] = "ENTER DATABASE IP OR DOMAINNAME";
+$GLOBALS['database_user'] = "ENTER DATABASE USER";
+$GLOBALS['database_password'] = "ENTER DATABASE PASSWORD";
+$GLOBALS['database_db'] = "ENTER DATABASE NAME";
+$GLOBALS['database_port'] = "ENTER DATABASE PORT";
